de139979a8b00f65bb219afa8f50141c63399805
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2021-11-19 17:23:06 +0300

    fiix bug from subdir service upload

--- setenv.go
+++ setenv.go
@@ -14,6 +14,7 @@ import (
 
 var (
 	logLevel, service, path string
+	isServices              bool
 )
 
 func main() {
@@ -30,10 +31,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	if service != "." {
+		isServices = true
+	}
 	for _, v := range dirs {
 		if v.IsDir() {
+			name := v.Name()
+			if isServices {
+				name = fmt.Sprintf("%s/%s", service, v.Name())
+			}
 			if v.Name() != "vendor" && v.Name() != path && v.Name() != ".git" {
-				err = filepath.WalkDir(v.Name(),
+				err = filepath.WalkDir(name,
 					func(filePath string, info os.DirEntry, err error) error {
 						if err != nil {
 							return err
@@ -44,17 +52,10 @@ func main() {
 								return err
 							}
 						} else {
-							readFile, err := ioutil.ReadFile(filePath)
+							writer(filePath, fmt.Sprintf("%s/%s", path, filePath))
 							if err != nil {
 								return err
 							}
-							data := []byte(os.ExpandEnv(string(readFile)))
-							writeFile, err := os.Create(fmt.Sprintf("%s/%s", path, filePath))
-							if err != nil {
-								return err
-							}
-							writeFile.Write(data)
-							writeFile.Close()
 						}
 						return nil
 					})
@@ -63,26 +64,36 @@ func main() {
 				}
 			}
 		} else {
-			err := os.MkdirAll(fmt.Sprintf("%s/%s", path, service), 0777)
-			if err != nil {
-				log.Error(err)
+			if isServices {
+				err := os.MkdirAll(fmt.Sprintf("%s/%s", path, service), 0777)
+				if err != nil {
+					log.Error(err)
+				}
+				writer(fmt.Sprintf("%s/%s", service, v.Name()), fmt.Sprintf("%s/%s/%s", path, service, v.Name()))
+				if err != nil {
+					log.Error(err)
+				}
 			}
-			readFile, err := ioutil.ReadFile(service + "/" + v.Name())
-			if err != nil {
-				log.Error(err)
-			}
-			data := []byte(os.ExpandEnv(string(readFile)))
-			writeFile, err := os.Create(fmt.Sprintf("%s/%s/%s", path, service, v.Name()))
-			if err != nil {
-				log.Error(err)
-			}
-			writeFile.Write(data)
-			writeFile.Close()
 		}
 	}
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func writer(r string, w string) error {
+	readFile, err := ioutil.ReadFile(r)
+	if err != nil {
+		return err
+	}
+	data := []byte(os.ExpandEnv(string(readFile)))
+	writeFile, err := os.Create(w)
+	if err != nil {
+		return err
+	}
+	writeFile.Write(data)
+	writeFile.Close()
+	return nil
 }
 
 func setLogLevel(level string) {
